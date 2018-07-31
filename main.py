c3f73da145a5a5f00011ba3ae408dec6453c8776
Demo User <demouser@MacBook-Pro.local>
2018-07-30 18:16:26 -0700

    added html info to main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from seed_stops_db import seed_data
 #this is in mph
 bus_speed = 26.4
 
-jina_env = jinja2.Environment(
+jinja_env = jinja2.Environment(
 loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
 extensions=['jinja2.ext.autoescape'],
 autoescape=True)
@@ -32,19 +32,27 @@ def find_time_to_stop(lat1, lng1, lat2, lng2):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        #self.response.write("Welcome to StopGo!")
-        welcome_template = jina_env.get_template('templates/main_page.html')
+        welcome_template = jinja_env.get_template('templates/homepage.html')
         self.response.write(welcome_template.render())
-
-
-
 
 
 class LoadDataHandler(webapp2.RequestHandler):
     def get(self):
         seed_data()
 
+class CreateRouteHandler(webapp2.RequestHandler):
+    def post(self):
+        create_route_template = jinja_env.get_template('templates/create_route.html')
+        self.response.write(create_route_template.render())
+
+class InformationHandler(webapp2.RequestHandler):
+    def post(self):
+        info_template = jinja_env.get_template('templates/information.html')
+        self.response.write(info_template.render())
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/seed-data', LoadDataHandler)
+    ('/seed-data', LoadDataHandler),
+    ('/create-route', CreateRouteHandler),
+    ('/info', InformationHandler),
 ], debug=True)
